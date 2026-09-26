4eaa2440c6a95d70f8f691426ae30fe549adae5e
Naing Vannath <naingvannath269@gmail.com>
2026-09-26 09:33:41 +0700

    Make the beautiful UI and fixed some bugs

--- anonymous_confession_proect/settings.py
+++ anonymous_confession_proect/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'confessiondb',
         'USER': 'postgres',
-        'PASSWORD': 'your_local_pss',
+        'PASSWORD': '200107',
         'HOST': 'localhost',
         'PORT': '5432',
     }
